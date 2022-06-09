3f935f8d8bc6d2cb7e46a7d1aa1411b806c44399
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-06-09 18:43:04 +0200

    card size correction

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -6,7 +6,9 @@
             <div class="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-6">
                 @foreach ($comics_list as $comic)
                     <div class="col">
-                        <img src="{{ $comic['thumb'] }}" alt="">
+                        <div class="card-img">
+                            <img src="{{ $comic['thumb'] }}" alt="">
+                        </div>
                         <h5>{{ $comic['title'] }}</h5>
                     </div>
                 @endforeach
